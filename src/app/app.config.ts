e1102746f681a39d275d42d69d2203764060055b
mohamedanwar120 <mohamed.anwar2002305@gmail.com>
2026-09-09 11:06:29 +0300

    resolve

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,11 @@ import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeuix/themes/aura';
 
+
 import { routes } from './app.routes';
+import {provideTranslateService} from "@ngx-translate/core";
+import {provideTranslateHttpLoader} from "@ngx-translate/http-loader";
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,6 +18,15 @@ export const appConfig: ApplicationConfig = {
       theme: {
         preset: Aura
       }
+    }),
+     provideHttpClient(withFetch()),
+    provideTranslateService({
+      loader: provideTranslateHttpLoader({
+        prefix: '/i18n/',
+        suffix: '.json'
+      }),
+      fallbackLang: 'ar',
+      lang: 'en'
     })
   ]
 };
